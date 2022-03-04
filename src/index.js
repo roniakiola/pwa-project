4871b84b762da0b0041748f428379b40403f45ab
kvtvs <kata.sara-aho@metropolia.fi>
2022-03-04 16:58:34 +0200

    automatic view change js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hello console!');
+'use strict';
+
+/*const createViewCarousel = (activeView) => {
+
+    const views = document.querySelectorAll('section');
+    for (const view of views){
+        view.style.display = 'none';
+    }
+    if (activeView === views.length){
+        activeView = 0;
+    }
+    views[activeView].style.display = 'block';
+    setTimeout(() => {
+        createViewCarousel(activeView + 1);    
+    }, 5000);
+};
+
+
+createViewCarousel(0);*/
