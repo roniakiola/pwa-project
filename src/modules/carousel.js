a759d5870df17bc9a607cefb31ae440ee5a3de04
casperdi <cassu.dikert@gmail.com>
2022-03-06 02:57:23 +0200

    carousel epilepsy fixed

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -1,52 +1,63 @@
 const carousel = () => {
 
-    const createViewCarousel = (activeView) => {
-        const views = document.querySelectorAll('section');
-        for (const view of views){
-          view.style.display = 'none';
-        }
-        if (activeView === views.length){
-          activeView = 0;
-        }
-        views[activeView].style.display = 'flex';
-        setTimeout(() => {
-          createViewCarousel(activeView + 1);    
-        }, 5000);
-      };
-      
-      
-      let counter = 0;
-      
-      const noMovement = () => {
-        if (counter === 15) {
-          createViewCarousel(0);
-          
-        } else {
-          console.log(counter);
-          counter++;
-        }
-      };
-      const resetTimer = () => {
-        counter = 0;
-      };
-      
-      window.onmousemove = () => {
-        resetTimer();
-      };
-      
-      window.onkeyup = () => {
-        resetTimer();
-      };
-      
-      window.ontouchstart = () => {
-        resetTimer();
-      };
-      
-      setInterval(() => {
-        noMovement();
-      }, 1000);
+  const createViewCarousel = (activeView) => {
+    const views = document.querySelectorAll('section');
+    for (const view of views) {
+      view.style.display = 'none';
+    }
+    if (activeView === views.length) {
+      activeView = 0;
+    }
+    views[activeView].style.display = 'flex';
+    setTimeout(() => {
+      createViewCarousel(activeView + 1);
+    }, 5000);
+  };
+
+
+  let counter = 0;
+  let stop = 0;
+
+  const noMovement = () => {
+    if (counter === 15) {
+      createViewCarousel(0);
+      counter++;
+      stop++;
+    } else {
+      console.log(counter);
+      counter++;
+    }
+  };
+  const resetTimer = () => {
+    counter = 0;
+  };
+
+  window.onmousemove = () => {
+    if (stop != 1){
+      resetTimer();
+    }
+  };
+
+  window.onkeyup = () => {
+    if (stop != 1){
+      resetTimer();
+    }
+  };
+
+  window.ontouchstart = () => {
+    if (stop != 1){
+      resetTimer();
+    }
+  };
+
+
+  setInterval(() => {
+    noMovement();
+  }, 1000);
+
+
+
 };
 
-export {carousel};
+export { carousel };
 
-  
