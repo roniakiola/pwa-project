668185875b86f1148968417cb1d1c9aba872c2a9
casperdi <cassu.dikert@gmail.com>
2022-03-06 21:09:01 +0200

    final touches to carousel

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -1,28 +1,33 @@
 const carousel = () => {
-
-  const createViewCarousel = (activeView) => {
-    const views = document.querySelectorAll('section');
-    for (const view of views) {
-      view.style.display = 'none';
-    }
-    if (activeView === views.length) {
-      activeView = 0;
-    }
-    views[activeView].style.display = 'flex';
-    setTimeout(() => {
-      createViewCarousel(activeView + 1);
-    }, 5000);
-  };
-
-
   let counter = 0;
   let stop = 0;
+  let carousel = false;
+
+  const createViewCarousel = (activeView) => {
+    if (carousel == true) {
+      const views = document.querySelectorAll('section');
+      for (const view of views) {
+        view.style.display = 'none';
+      }
+      if (activeView === views.length) {
+        activeView = 0;
+      }
+      views[activeView].style.display = 'flex';
+
+      setTimeout(() => {
+        createViewCarousel(activeView + 1);
+      }, 20000);
+    }
+  };
 
   const noMovement = () => {
-    if (counter === 15) {
-      createViewCarousel(0);
+    if (counter === 60) {
+      carousel = true;
       counter++;
       stop++;
+      const hamburger = document.querySelector('.hamburger-menu');
+      hamburger.classList.add('hidden');
+      createViewCarousel(0);
     } else {
       console.log(counter);
       counter++;
@@ -33,30 +38,39 @@ const carousel = () => {
   };
 
   window.onmousemove = () => {
-    if (stop != 1){
+    if (stop != 1) {
       resetTimer();
     }
   };
 
   window.onkeyup = () => {
-    if (stop != 1){
+    if (stop != 1) {
       resetTimer();
     }
   };
 
-  window.ontouchstart = () => {
-    if (stop != 1){
+  window.ontouchmove = () => {
+    if (stop != 1) {
       resetTimer();
     }
   };
 
-
+  window.onkeydown = (e) => {
+    const views = document.querySelectorAll('section');
+    if (e.keyCode == 27) {
+      for (const view of views) {
+        view.style.display = 'flex';
+      }
+      stop = 0;
+      carousel = false;
+      const hamburger = document.querySelector('.hamburger-menu');
+      hamburger.classList.remove('hidden');
+      resetTimer();
+    }
+  };
   setInterval(() => {
     noMovement();
   }, 1000);
-
-
-
 };
 
 export { carousel };
